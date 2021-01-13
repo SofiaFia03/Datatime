0d0d0f9245cd6ecd0aa657a6af4561273a579cf4
SofiaFia03 <74136529+SofiaFia03@users.noreply.github.com>
2021-01-12 19:04:44 -0800

    Add files via upload

---
+++ Datetime/src/com/dicoding/javafundamental/datetime/ExampleSystemCurrentMilis.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dicoding.javafundamental.datetime;
+
+/**
+ *
+ * @author USER
+ */
+public class ExampleSystemCurrentMilis {
+    public static void main(String[] args) {
+        long timeNow = System.currentTimeMillis();
+        System.out.println("Waktu sekarang adalah " + timeNow + " milisecond");
+    }
+    
+}
